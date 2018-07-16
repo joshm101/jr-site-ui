e554f62083127de01502a3ec0ba9ae9f7b57259e
Joshua Mora <jshm101@gmail.com>
2018-07-16 00:50:15 -0700

    environment variable usage & configuration

--- src/index.js
+++ src/index.js
@@ -6,11 +6,30 @@ import store from './store'
 import './index.css'
 import App from './App'
 
+const USERS_API_URL = process.env.REACT_APP_USERS_API_URL
+
+const validEnv = () => (
+  !!(USERS_API_URL)
+)
+
 ReactDOM.render(
-  (
-    <Provider store={store}>
-      <App />
-    </Provider>
+  (validEnv()
+    ? (
+      <Provider store={store}>
+        <App />
+      </Provider>
+    ) : (
+      <div className="error-wrapper">
+        <div className="error">
+          <h2>
+            Necessary environment variables not set.
+          </h2>
+          <h3>
+            This application cannot start.
+          </h3>
+        </div>
+      </div>
+    )
   ),
   document.getElementById('root')
 )
