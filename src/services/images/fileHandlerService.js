177a77ea4205c97f7e55db1e3ae1288a67f36381
Joshua Mora <jshm101@gmail.com>
2018-08-04 18:29:06 -0700

    services to facilitate image file uploads

---
+++ src/services/images/fileHandlerService.js
@@ -0,0 +1,31 @@
+let instance = null
+
+/**
+ * Factory function closure with private
+ * filesToUpload value that can be
+ * manipulated via returned object's
+ * property functions. Cannot store File
+ * objects in Redux state, so this is
+ * used instead. Maintains a references to
+ * an instance variable to ensure singleton
+ * behavior
+ */
+const fileHandlerService = () => {
+  let filesToUpload = []
+  if (!instance) {
+    // instance not yet created, create
+    // service object instance
+    instance = Object.create({
+      setFiles: (files) => {
+        filesToUpload = [...files]
+      },
+      getFiles: () => {
+        return filesToUpload
+      }
+    })
+  }
+  // Always return single instance
+  return instance
+}
+
+export default fileHandlerService
--- src/services/images/index.js
+++ src/services/images/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
 import { handleAxiosRequestError } from '../utils'
+import validImageFile from '../../utils/valid-image-file'
 
 const IMAGES_API_URL = process.env.REACT_APP_IMAGES_API_URL
 
@@ -18,6 +19,68 @@ const getImages = () => {
   ).catch(handleAxiosRequestError)
 }
 
+/**
+ * Fires a network request to API to upload the provided
+ * image files to the specified folder
+ * @param {File[]} imageFiles - Array of File objects
+ * @param {string} folder - Name of folder to upload images to
+ * @return {Promise<object>} - API response object
+ */
+const uploadImages = (imageFiles, folder) => {
+  if (!folder) {
+    throw new Error(
+      'A folder must be specified for the uploaded images.'
+    )
+  }
+
+  const authToken = localStorage.getItem('jr-site-auth-token')
+  if (!authToken) {
+    throw new Error(
+      'An auth token was not found. Not authorized to upload images.'
+    )
+  }
+
+  // Filter out any invalid image files as a safeguard
+  const validImageFiles = imageFiles.filter(imageFile =>
+    validImageFile(imageFile.name)
+  )
+
+  // Get invalid image files for use in response to consumer
+  const invalidImageFiles = imageFiles.filter(imageFile =>
+    !validImageFile(imageFile.name)
+  )
+
+  // Append folder and image files to FormData object
+  const formData = new FormData()
+  formData.append('folder', folder)
+  validImageFiles.forEach(imageFile =>
+    formData.append('images', imageFile)
+  )
+
+  const requestOptions = {
+    method: 'POST',
+    headers: {
+      'Authorization': `Bearer: ${authToken}`
+    },
+    data: formData,
+    url: `${IMAGES_API_URL}/upload-images`
+  }
+
+  // Make POST request to upload images.
+  return axios(requestOptions).then(response =>
+    ({
+      invalidFiles: [
+        ...response.data.data.invalidFiles,
+        ...invalidImageFiles
+      ],
+      uploadErrors: response.data.data.uploadErrors,
+      uploadedFiles: response.data.data.uploadedFiles
+
+    })
+  ).catch(handleAxiosRequestError)
+}
+
 export {
-  getImages
+  getImages,
+  uploadImages
 }
