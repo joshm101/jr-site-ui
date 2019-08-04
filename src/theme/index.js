de9c45b7eaa14fab81a230cdbd195ee10f97ba5a
Joshua Mora <jshm101@gmail.com>
2019-08-03 18:19:59 -0700

    update material-ui theme

--- src/theme/index.js
+++ src/theme/index.js
@@ -2,25 +2,25 @@ import { createMuiTheme } from '@material-ui/core/styles'
 
 const palette = {
   primary: {
-    main: '#16161d',
-    light: '#575773',
-    dark: '#040406',
+    main: '#263238',
+    light: '#4f5b62',
+    dark: '#000a12',
     contrastText: '#fff'
   },
   secondary: {
-    main: '#74848E',
-    light: '#A9BFCC',
-    dark: '#3C5769',
-    contrastText: '#fff'
+    main: '#ffe082',
+    light: '#ffffb3',
+    dark: '#caae53',
+    contrastText: '#000000'
   },
   background: {
     main: '#f2f2f2',
     light: '#fafafa'
   },
   error: {
-    main: '#7e2c2c',
-    light: '#D17D7D',
-    dark: '#6E0D00',
+    main: '#e57373',
+    light: '#ffa4a2',
+    dark: '#af4448',
     contrastText: '#fff'
   }
 }
