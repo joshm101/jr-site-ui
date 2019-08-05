72eed1083168b76b228a1ecdd7d989a38b2b0522
Joshua Mora <jshm101@gmail.com>
2019-08-04 21:47:57 -0700

    remove Card usage

--- src/routes/Interface/Content/ViewPost/index.js
+++ src/routes/Interface/Content/ViewPost/index.js
@@ -1,10 +1,6 @@
 import React from 'react'
 import {
   Typography,
-  Card,
-  CardHeader,
-  CardContent,
-  CardActions,
   Button,
   Icon
 } from '@material-ui/core'
@@ -17,30 +13,22 @@ const ViewPost = ({ match, posts }) => {
   const post = posts.data.find(post => post._id === _id)
   return (
     <ContentContainer className="interface-view-post-root">
-      <Card elevation={0}>
-        <CardHeader
-          title={
-            <Typography variant="h5">{post.title}</Typography>
-          }
-        />
-        <CardContent>
-          <div className="interface-view-post-images">
-            <div className="interface-view-post-thumbnail">
-              <img src={post.thumbnail} />
-            </div>
-          </div>
-          <div className="interface-view-post-description">
-            <Typography>
-              {post.description}
-            </Typography>
-          </div>
-        </CardContent>
-        <CardActions className="interface-view-post-actions">
-          <Button variant="text" color="primary">
-            <Icon fontSize="inherit">edit</Icon>&nbsp;&nbsp;Edit
-          </Button>
-        </CardActions>
-      </Card>
+      <Typography variant="h3" color="textPrimary">
+        {post.title}
+      </Typography>
+      <div className="interface-view-post-images">
+        <div className="interface-view-post-thumbnail">
+          <img src={post.thumbnail} />
+        </div>
+      </div>
+      <div className="interface-view-post-description">
+        <Typography>
+          {post.description}
+        </Typography>
+      </div>
+      <Button variant="text" color="primary">
+        <Icon fontSize="inherit">edit</Icon>&nbsp;&nbsp;Edit
+      </Button>
     </ContentContainer>
   )
 }
