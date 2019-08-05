b80608cb554204f86941ba3dea89a551564158e3
Joshua Mora <jshm101@gmail.com>
2019-08-05 01:07:24 -0700

    use ContentHeader component on ViewPost route

--- src/routes/Interface/Content/ViewPost/ViewPost.js
+++ src/routes/Interface/Content/ViewPost/ViewPost.js
@@ -4,6 +4,7 @@ import {
   Icon
 } from '@material-ui/core'
 import ContentContainer from '../ContentContainer'
+import ContentHeader from '../ContentHeader'
 import PostDetails from '../../../../components/Interface/PostDetails'
 
 import withPosts from '../../../../hoc/withPosts'
@@ -11,17 +12,25 @@ import withPosts from '../../../../hoc/withPosts'
 const ViewPost = ({ match, posts }) => {
   const _id = parseInt(match.params._id)
   const post = posts.data.find(post => post._id === _id)
+
   return (
-    <ContentContainer className="interface-view-post-root">
-      <PostDetails post={post}>
-        <PostDetails.Title />
-        <PostDetails.Images />
-        <PostDetails.Description />
-      </PostDetails>
-      <Button variant="text" color="secondary">
-        <Icon fontSize="inherit">edit</Icon>&nbsp;&nbsp;Edit
-      </Button>
-    </ContentContainer>
+    <>
+      <ContentHeader
+        title="Post Details"
+        action={
+          <Button variant="text" size="large" color="secondary">
+            <Icon fontSize="inherit">edit</Icon>&nbsp;&nbsp;Edit Post
+          </Button>
+        }
+      />
+      <ContentContainer className="interface-view-post-root">
+        <PostDetails post={post}>
+          <PostDetails.Title />
+          <PostDetails.Images />
+          <PostDetails.Description />
+        </PostDetails>
+      </ContentContainer>
+    </>
   )
 }
 
