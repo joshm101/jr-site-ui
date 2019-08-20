e35f8a2df143406255189d5d3e4a2a2d533007dd
Joshua Mora <jshm101@gmail.com>
2019-08-19 21:34:23 -0700

    selectable image component for image selection ui (e.g., post forms, image CRUD, etc.)

---
+++ src/components/Interface/SelectableImage/ImageOverlay/styles.js
@@ -0,0 +1,20 @@
+const styles = () => ({
+  root: {
+    backgroundColor: '#000',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    height: '100%',
+    width: '100%'
+  },
+  transparent: {
+    opacity: 0
+  },
+  dark: {
+    opacity: 0.4
+  }
+})
+
+export default styles
