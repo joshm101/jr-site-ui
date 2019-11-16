f1fe435ac7204b9423509097e8321a6ed181fca5
Joshua Mora <jshm101@gmail.com>
2019-11-15 20:22:13 -0800

    feat: add useUploadImages hook
    Hook provides upload images state & actions to consumers

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,3 +1,4 @@
 export { default as useImages } from './useImages'
 export { default as useImagesActions } from './useImagesActions'
 export { default as usePosts } from './usePosts'
+export { default as useUploadImages } from './useUploadImages'
---
+++ src/hooks/useUploadImages.js
@@ -0,0 +1,63 @@
+import { useSelector, useDispatch } from 'react-redux'
+
+import {
+  uploadImagesImageSelection,
+  uploadImagesInvalidFilesNoticeDismissed,
+  uploadImagesRoutine,
+  uploadImagesSelectFolder,
+  uploadImagesDefineNewFolderTrigger,
+  uploadImagesDefineNewFolderCancel,
+  uploadImagesDefineNewFolderValueChange,
+  uploadImagesDefineNewFolderSubmit,
+  uploadImagesRemoveImage,
+  dismissUploadImagesSuccessNotice,
+  dismissUploadImagesFailureNotice
+} from '../actions'
+
+function useUploadImages() {
+  const uploadImagesState = useSelector(state => state.uploadImages)
+  const dispatch = useDispatch()
+
+  const uploadImagesActions = {
+    uploadImagesImageSelection: (...args) => dispatch(
+      uploadImagesImageSelection(...args)
+    ),
+    uploadImagesInvalidFilesNoticeDismissed: (...args) => dispatch(
+      uploadImagesInvalidFilesNoticeDismissed(...args)
+    ),
+    uploadImagesRoutine: (...args) => dispatch(
+      uploadImagesRoutine(...args)
+    ),
+    uploadImagesSelectFolder: (...args) => dispatch(
+      uploadImagesSelectFolder(...args)
+    ),
+    uploadImagesDefineNewFolderTrigger: (...args) => dispatch(
+      uploadImagesDefineNewFolderTrigger(...args)
+    ),
+    uploadImagesDefineNewFolderCancel: (...args) => dispatch(
+      uploadImagesDefineNewFolderCancel(...args)
+    ),
+    uploadImagesDefineNewFolderValueChange: (...args) => dispatch(
+      uploadImagesDefineNewFolderValueChange(...args)
+    ),
+    uploadImagesDefineNewFolderSubmit: (...args) => dispatch(
+      uploadImagesDefineNewFolderSubmit(...args)
+    ),
+    uploadImagesRemoveImage: (...args) => dispatch(
+      uploadImagesRemoveImage(...args)
+    ),
+    dismissUploadImagesSuccessNotice: (...args) => dispatch(
+      dismissUploadImagesSuccessNotice(...args)
+    ),
+    dismissUploadImagesFailureNotice: (...args) => dispatch(
+      dismissUploadImagesFailureNotice(...args)
+    )
+  }
+
+  return {
+    state: uploadImagesState,
+    actions: uploadImagesActions
+  }
+}
+
+export default useUploadImages
