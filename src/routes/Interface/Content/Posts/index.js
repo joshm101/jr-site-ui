b348442c3863c89cfe0bb8d5fe2c1828b979fa80
Joshua Mora <jshm101@gmail.com>
2019-11-15 19:37:47 -0800

    refactor: replace withPosts hoc with usePosts hook
    Replaces withPosts hoc in Posts and ViewPost components with usePosts hook. Deletes withPosts hoc

--- src/routes/Interface/Content/Posts/index.js
+++ src/routes/Interface/Content/Posts/index.js
@@ -2,22 +2,26 @@ import React from 'react'
 
 import Post from '../../../../components/Interface/Post'
 import ContentContainer from '../ContentContainer'
-import withPosts from '../../../../hoc/withPosts'
+import { usePosts } from '../../../../hooks'
 
 import '../index.css'
 
-const Posts = ({ posts }) => (
-  <ContentContainer className="interface-posts-container">
-    {posts.data.length > 0 &&
-      posts.data.map((post, index) =>
-        <Post
-          key={post._id}
-          post={post}
-          className={`interface-post-${index}`}
-        />
-      )
-    }
-  </ContentContainer>
-)
+function Posts() {
+  const posts = usePosts()
 
-export default withPosts(Posts)
+  return (
+    <ContentContainer className="interface-posts-container">
+      {posts.data.length > 0 &&
+        posts.data.map((post, index) =>
+          <Post
+            key={post._id}
+            post={post}
+            className={`interface-post-${index}`}
+          />
+        )
+      }
+    </ContentContainer>
+  )
+}
+
+export default Posts
