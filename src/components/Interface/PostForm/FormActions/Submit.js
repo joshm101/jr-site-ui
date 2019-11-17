3ab3e6a6f3617c015d25e4c113949766691cd839
Joshua Mora <jshm101@gmail.com>
2019-11-17 15:17:10 -0800

    chore: add data-test-id prop support on Submit component
    Adds data-test-id prop to Submit component so that it can be reliably
    queried/identified in tests

--- src/components/Interface/PostForm/FormActions/Submit.js
+++ src/components/Interface/PostForm/FormActions/Submit.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import Button from '@material-ui/core/Button'
 
-function Submit({ onClick, disabled, className }) {
+function Submit({
+  onClick,
+  disabled,
+  className,
+  'data-test-id': dataTestId
+}) {
   return (
     <Button
+      data-test-id={dataTestId}
       onClick={onClick}
       disabled={disabled}
       className={className}
