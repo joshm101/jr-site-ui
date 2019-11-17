0b3e8bd27f43303f0c8bd18be1b3b6bf391978e4
Joshua Mora <jshm101@gmail.com>
2019-11-17 15:58:00 -0800

    chore: add data-test-id prop support on FormErrors component
    Adds data-test-id prop to FormErrors component so that form errors can
    be reliably queried/identified in tests

--- src/components/Interface/PostForm/FormErrors/FormErrors.js
+++ src/components/Interface/PostForm/FormErrors/FormErrors.js
@@ -2,13 +2,19 @@ import React from 'react'
 import Typography from '@material-ui/core/Typography'
 
 function FormErrors(props) {
-  const { errors, className, errorClassName } = props
+  const {
+    errors,
+    className,
+    errorClassName,
+    'data-test-id': dataTestId
+  } = props
 
   return (
     <div className={className}>
       {errors && errors.length &&
         errors.map(error =>
           <Typography
+            data-test-id={dataTestId}
             key={error}
             variant="caption"
             className={errorClassName}
