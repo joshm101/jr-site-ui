b9781a7b1288221e76c7fe46a37a8002abfaef2c
Joshua Mora <jshm101@gmail.com>
2019-11-22 22:28:29 -0800

    feat: adds loading indicator to post form submit button
    Adds a loading indicator to post form submit button that is
    displayed while the form is submitting (while a post is being
    created/updated). Submit button is disabled while form is submitting.

--- src/components/Interface/PostForm/FormActions/Submit.js
+++ src/components/Interface/PostForm/FormActions/Submit.js
@@ -1,22 +1,41 @@
 import React from 'react'
+import classname from 'classnames'
+import { makeStyles } from '@material-ui/styles'
 import Button from '@material-ui/core/Button'
+import CircularProgress from '@material-ui/core/CircularProgress'
+import styles from './Submit.styles'
+
+const useStyles = makeStyles(styles)
 
 function Submit({
   onClick,
   disabled,
+  submitting,
+  children,
   className,
   'data-test-id': dataTestId
 }) {
+  const classes = useStyles()
+  const rootClasses = classname([classes.root, className])
+
+  const loadingIcon = submitting && (
+    <CircularProgress
+      size={16}
+      data-test-id="post-form-submit-indicator"
+    />
+  )
+
   return (
     <Button
       data-test-id={dataTestId}
       onClick={onClick}
-      disabled={disabled}
-      className={className}
+      disabled={disabled || submitting}
+      className={rootClasses}
       color="secondary"
       variant="outlined"
+      startIcon={loadingIcon}
     >
-      Done
+      {children}
     </Button>
   )
 }
