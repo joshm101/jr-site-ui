b2f616d29692f214d0422b88057788a3ad8a575a
Joshua Mora <jshm101@gmail.com>
2019-11-22 19:32:04 -0800

    chore: use material-ui Container component

--- src/routes/Interface/Content/index.js
+++ src/routes/Interface/Content/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
+import Container from '@material-ui/core/Container'
 
 import Posts from './Posts'
 import ViewPost from './ViewPost'
@@ -10,13 +11,13 @@ import CreatePost from './CreatePost'
 import './index.css'
 
 const Content = ({ match, posts }) => (
-  <div className="interface-content-routes-container">
+  <Container>
     <Route exact path={`${match.url}/`} component={Posts} />
     <Route exact path={`${match.url}/create-post`} component={CreatePost} />
     <Route path={`${match.url}/view-post/:_id`} component={ViewPost} />
     <Route exact path={`${match.url}/images`} component={Images} />
     <Route path={`${match.url}/images/upload`} component={UploadImages} />
-  </div>
+  </Container>
 )
 
 export default Content
