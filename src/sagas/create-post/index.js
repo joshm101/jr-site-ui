0f24aaf3a72ad8595270ed86e977ca78aa5e6a69
Joshua Mora <jshm101@gmail.com>
2019-12-08 23:38:52 -0800

    feat: create post success redirect
    Redirects user to interface home/dashboard when a post has been
    successfully created.

--- src/sagas/create-post/index.js
+++ src/sagas/create-post/index.js
@@ -1,4 +1,5 @@
 import { takeEvery, call, put } from 'redux-saga/effects'
+import { push } from 'connected-react-router'
 
 import {
   createPostRoutine,
@@ -7,6 +8,7 @@ import {
 import * as postsService from '../../services/posts'
 
 import { SUCCESS_NOTIFICATION_ID } from '../../components/Interface/PostForm'
+import { ROUTES } from '../../routes/routes.constants'
 
 function* handleCreatePost(action) {
   const { payload } = action
@@ -26,6 +28,10 @@ function* handleCreatePost(action) {
         id: SUCCESS_NOTIFICATION_ID,
         props: { message: 'The post was successfully created!' }
       })
+    )
+
+    yield put(
+      push(`/${ROUTES.INTERFACE}`)
     )
   } catch (error) {
     yield put(
