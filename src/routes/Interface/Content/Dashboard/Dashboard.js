c51478c5bf3c0b989bd1fc4e49e453016df78635
Joshua Mora <jshm101@gmail.com>
2019-12-17 09:49:32 -0800

    feat: posts list components
    Adds components for displaying list of posts from application state

--- src/routes/Interface/Content/Dashboard/Dashboard.js
+++ src/routes/Interface/Content/Dashboard/Dashboard.js
@@ -1,9 +1,12 @@
 import React from 'react'
 
+import PostsList from '../../../../components/Interface/PostsList'
+
 function Dashboard() {
   return (
     <>
       <h1>Dashboard</h1>
+      <PostsList />
     </>
   )
 }
